7714854c5ff8dc8888aa479b123d8a262c124cd5
Alessa <48030143+wolframchik@users.noreply.github.com>
2019-05-27 15:32:43 +0300

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ sock = socket.socket()
 sock.connect(('127.0.0.1', 5000))
 matrix = []
 for i in range(0,3):
-    matrix.append(input(f'Введите {i+1}-ю строку: '))
+    matrix.append(input(f'Введите {i+1}-ю строку(через пробел): '))
 
 
 sock.send(str(matrix).encode())
